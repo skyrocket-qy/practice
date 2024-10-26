2f21418e8cd192d2e31bc6e97282afcb40692737
Jimmy <rivendinner@gmail.com>
2024-10-27 02:14:34 +0800

    backup

--- Go/raft/go.mod
+++
@@ -1,3 +0,0 @@
-module program3
-
-go 1.21.6
--- Go/raid/domain.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Raid interface {
-	WriteString(data string) error
-	ClearDisk(diskIndex int)
-	ReadString(length int) (string, error)
-}
